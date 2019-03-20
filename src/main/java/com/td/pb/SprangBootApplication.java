bb3c6d7a3abe8cf2530d7e92b72fc83d13195bc2
Ananda Dhandapani <Ananda.Dhandapani@td.com>
2019-03-20 12:33:13 -0400

    Added initial files

---
+++ src/main/java/com/td/pb/SprangBootApplication.java
@@ -0,0 +1,13 @@
+package com.td.pb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprangBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprangBootApplication.class, args);
+	}
+
+}
